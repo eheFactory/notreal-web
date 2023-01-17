955dbc6f248e82bdeac432db5814e58fc52e18b1
barannkuzu <barannkuzu@gmail.com>
2023-01-17 16:20:49 +0300

    assets and last working app

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,52 +1,63 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const scene = new THREE.Scene()
+export class App{
+    camera: THREE.PerspectiveCamera;
+    scene: THREE.Scene;
+    renderer: THREE.WebGLRenderer;
+    mesh: THREE.Mesh;
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+        
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+		this.camera.position.set( 0, 0, 4 );
+        
+		this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0xaaaaaa );
 
-camera.position.z = 2
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+		this.scene.add(ambient);
+        
+        const light = new THREE.DirectionalLight();
+        light.position.set( 0.2, 1, 1);
+        this.scene.add(light);
+			
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		container.appendChild( this.renderer.domElement );
+		
+        //Replace Box with Circle, Cone, Cylinder, Dodecahedron, Icosahedron, Octahedron, Plane, Sphere, Tetrahedron, Torus or TorusKnot
+        const geometry = new THREE.BoxGeometry(); 
+        
+        const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 });
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-new OrbitControls(camera, renderer.domElement)
-
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+        this.mesh = new THREE.Mesh( geometry, material );
+        
+        this.scene.add(this.mesh);
+        
+        const controls = new OrbitControls( this.camera, this.renderer.domElement );
+        
+        this.renderer.setAnimationLoop(this.render.bind(this));
+    
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+    
+	render( ) {   
+        this.mesh.rotateY( 0.01 );
+        this.renderer.render( this.scene, this.camera );
+    }
 }
 
-function animate() {
-    requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
-    render()
-}
-
-function render() {
-    renderer.render(scene, camera)
-}
-
-animate()
+document.addEventListener("DOMContentLoaded", function () {
+    const app = new App();
+    (window as any).app = app;
+});
