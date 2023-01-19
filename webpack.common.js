9e5d071585965325cebacbea0f1685ca847b53f6
barannkuzu <barannkuzu@gmail.com>
2023-01-19 03:11:20 +0300

    test2

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,24 @@
 const path = require('path')
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: './src/entry.js',
     module: {
         rules: [
             {
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.(glb|gltf)$/,
+                include: path.resolve(__dirname, './src/assets'),
+                use: [
+                 {
+                  loader: 'file-loader',
+                  options: {
+                   outputPath: 'assets/',
+                   sourceMap: true
+                  }
+                }
+            ]
             },
         ],
     },
@@ -16,4 +29,9 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './'),
     },
+    plugins: [
+        new CopyWebpackPlugin({
+         patterns: [{ from: './src/assets', to:'./assets' }]
+        })
+    ]
 }
