5189081c8b9bb9d4f76d03ed00b97b9e4ae4f30c
barannkuzu <barannkuzu@gmail.com>
2023-01-22 17:37:56 +0300

    testing

--- src/entry.js
+++ src/entry.js
@@ -31,12 +31,16 @@ class App {
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0x505050);
 
-        this.scene.add(new THREE.HemisphereLight(0x606060, 0x404040));
+        const ambient = new THREE.HemisphereLight(0x606060, 0x404040);
+        // const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+        this.scene.add(ambient);
 
         const light = new THREE.DirectionalLight(0xffffff);
+        // light.position.set( 0.2, 1, 1);
         light.position.set(1, 1, 1).normalize();
         this.scene.add(light);
 
+        // this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -49,7 +53,14 @@ class App {
         this.controls.update();
 
         this.stats = new Stats();
-        document.body.appendChild(this.stats.dom);
+        container.appendChild(this.stats.dom);
+
+        //Replace Box with Circle, Cone, Cylinder, Dodecahedron, Icosahedron, Octahedron, Plane, Sphere, Tetrahedron, Torus or TorusKnot
+        // const geometry = new THREE.BoxGeometry();
+        // const material = new THREE.MeshStandardMaterial({ color: 0xFF0000 });
+        // this.mesh = new THREE.Mesh(geometry, material);
+        // this.scene.add(this.mesh);
+
 
         this.raycaster = new THREE.Raycaster();
         this.workingMatrix = new THREE.Matrix4();
@@ -71,6 +82,7 @@ class App {
         this.radius = 0.08;
 
         this.room = new THREE.LineSegments(
+            // new THREE.BoxGeometry(6,6,6,10,10,10),
             new BoxLineGeometry(6, 6, 6, 10, 10, 10),
             new THREE.LineBasicMaterial({ color: 0x808080 })
         );
@@ -91,6 +103,8 @@ class App {
 
         }
 
+        // side: THREE.FrontSide
+        // side: THREE.BothSide
         this.highlight = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.BackSide }));
         this.highlight.scale.set(1.2, 1.2, 1.2);
         this.scene.add(this.highlight);
@@ -166,11 +180,8 @@ class App {
 
     setupXR() {
         this.renderer.xr.enabled = true;
-
+        // document.body.appendChild(VRButton.createButton(this.renderer));
         const button = new VRButton(this.renderer);
-
-        const self = this;
-
         function onConnected(event) {
             const info = {};
 
@@ -188,11 +199,11 @@ class App {
                     info[key] = components;
                 });
 
-                self.createButtonStates(info.right);
+                this.createButtonStates(info.right);
 
                 console.log(JSON.stringify(info));
 
-                self.updateControllers(info);
+                this.updateControllers(info);
 
             });
         }
@@ -236,7 +247,6 @@ class App {
     }
 
     updateControllers(info) {
-        const self = this;
 
         function onSelectStart() {
             this.userData.selectPressed = true;
@@ -259,7 +269,7 @@ class App {
         function onSqueezeEnd() {
             this.userData.squeezePressed = false;
             if (this.userData.attachedObject !== undefined) {
-                self.room.attach(this.userData.attachedObject);
+                this.room.attach(this.userData.attachedObject);
                 this.userData.attachedObject = undefined;
             }
         }
@@ -268,16 +278,16 @@ class App {
             const index = this.userData.index;
             console.log(`Disconnected controller ${index}`);
 
-            if (self.controllers) {
-                const obj = (index == 0) ? self.controllers.right : self.controllers.left;
+            if (this.controllers) {
+                const obj = (index == 0) ? this.controllers.right : this.controllers.left;
 
                 if (obj) {
                     if (obj.controller) {
                         const controller = obj.controller;
                         while (controller.children.length > 0) controller.remove(controller.children[0]);
-                        self.scene.remove(controller);
+                        this.scene.remove(controller);
                     }
-                    if (obj.grip) self.scene.remove(obj.grip);
+                    if (obj.grip) this.scene.remove(obj.grip);
                 }
             }
         }
@@ -357,12 +367,12 @@ class App {
 
     render() {
         const dt = this.clock.getDelta();
+        // this.mesh.rotateY(0.01);
 
         if (this.renderer.xr.isPresenting) {
-            const self = this;
             if (this.controllers) {
                 Object.values(this.controllers).forEach((value) => {
-                    self.handleController(value.controller);
+                    this.handleController(value.controller);
                 });
             }
             if (this.elapsedTime === undefined) this.elapsedTime = 0;
